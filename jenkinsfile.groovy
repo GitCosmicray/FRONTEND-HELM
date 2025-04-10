5b229153c720e3b28e2aa6966d943d10d8b1bc0b
GitCosmicray <94951603+GitCosmicray@users.noreply.github.com>
2025-04-10 06:24:42 +0000

    modified groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -8,9 +8,9 @@ pipeline {
     IMAGE_NAME = 'frontend-app'
     TAG = "v${BUILD_NUMBER}"
     GCP_KEY = credentials('GCP-jenkins-json') 
-    PAT_TOKEN = credentials('PAT-access-github')
+    // PAT_TOKEN = credentials('PAT-access-github')
   }
-  
+
   stages {
     stage('Clone Repo') {
       steps {
@@ -38,18 +38,20 @@ pipeline {
       }
     }
 
-    stage('Clone Helm Repo and Update Tag') {
+     stage('Clone Helm Repo and Update Tag') {
       steps {
-        sh '''
-          git clone https://github.com/GitCosmicray/FRONTEND-HELM.git
-          cd FRONTEND-HELM/frontend-helmm
-          sed -i 's|tag:.*|tag: "${TAG}"|' values.yaml
-          git config user.name "jenkins-bot"
-          git config user.email "jenkins@yourcompany.com"
-          git add values.yaml
-          git commit -m "Update image tag to ${TAG}"
-          git push https://${PAT_TOKEN}@github.com/GitCosmicray/FRONTEND-HELM.git
-        '''
+        withCredentials([string(credentialsId: 'PAT-access-github', variable: 'TOKEN')]) {
+          sh '''
+            git clone https://$TOKEN@github.com/GitCosmicray/FRONTEND-HELM.git
+            cd FRONTEND-HELM/frontend-helmm
+            sed -i 's|tag:.*|tag: "'$TAG'"|' values.yaml
+            git config user.name "jenkins-bot"
+            git config user.email "jenkins@yourcompany.com"
+            git add values.yaml
+            git commit -m "Update image tag to $TAG"
+            git push https://$TOKEN@github.com/GitCosmicray/FRONTEND-HELM.git
+          '''
+        }
       }
     }
   }
